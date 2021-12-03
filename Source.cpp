880e80dd3ae86bb5c43019d7fa6b6f11fe9b53b6
Ryankhoury <92649927+Ryankhoury@users.noreply.github.com>
2021-12-02 23:06:40 -0500

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,78 +1,134 @@
 #include <iostream>
-#include <iomanip> 
-#include <cctype>
+
 using namespace std;
 
-
-int s = 0;
-// three function one to show usage
-void ShowUsage(){
+void ShowUsage()
+{
     cout<<"To show program usage 'S'"<<endl;
     cout<<"To purchase a surfboard 'P'"<<endl;
     cout<<"To display current purchase press 'C'"<<endl;
     cout<<"To display total amount due press 'T'"<<endl;
     cout<<"To quit the program press 'Q'"<<endl;
 }
-// make purchase function that take the quantity of the purchased surfboard 
-void MakePurchase(int& iTotalSmall, int& iTotalMedium, int& iTotalLarge){
-    int quantity;
-    
-    cout<<"Please enter the quantity surfboard you would like to purchase:";
-    cin>>quantity;
-   
-            s+=quantity;
-        
-}
-//display function that display the quantity of the purchased surfboards
-void DisplayPurchase(const int iTotalSmall){
-    if(s!=0){
-        cout<<"The total number of surfboard is "<<s<<endl;
-    }iif(s==0 ){
-        cout<<"No purchase made yet.";
+
+void MakePurchase(int &iTotalSmall, int &iTotalMedium, int &iTotalLarge, int &iTotalXXS)
+{
+    int quantity ;
+    char selection ;
+
+    cout<<"Please enter the quantity and type (S=small , M=medium , L=Large ,X = XXS) of the surfboard you would like to purchase : "<<endl ;
+    cin>>quantity ;
+    cin>>selection ;
+
+    switch(tolower(selection))
+    {
+    case 's':
+        iTotalSmall += quantity ;
+        break ;
+    case'm' :
+        iTotalMedium += quantity ;
+        break ;
+    case'l' :
+        iTotalLarge += quantity ;
+        break ;
+    case'x' :
+        iTotalXXS += quantity ;
+        break ;
+
     }
 }
-//DisplayTotal functionthat display the quantity of surfboard purchased and ther total amount of money in dollar 
-void DisplayTotal(const int iTotalSmall){
-    int sum = 0 ;
-     if(s!=0){
-        cout<<"The total number of small surfboard is "<<s<<" at a total of $"<<(s*iTotalSmall)<<endl;
-        sum+=(s*iTotalSmall);
+
+void DisplayPurchase(const int iTotalSmall, const int iTotalMedium, const int iTotalLarge, const int iTotalXXS)
+{
+    if((iTotalSmall== 0)&&(iTotalMedium==0)&&(iTotalLarge==0)&&(iTotalXXS==0))
+    {
+        cout<<"No purchases made yet."<<endl ;
     }
-    if(s==0 ){
-        cout<<"No purchase made yet.";
+    if(iTotalSmall!=0)
+    {
+        cout<<"The total number of small surfboards is "<<iTotalSmall<<endl ;
     }
-    cout<<"Amount due $"<<(sum)<<endl;
+    if(iTotalMedium!=0)
+    {
+        cout<<"The total number of medium surfboards is "<<iTotalMedium<<endl ;
+    }
+    if(iTotalLarge!=0)
+    {
+        cout<<"The total number of large surfboards is "<<iTotalLarge<<endl ;
+    }
+    if(iTotalXXS!=0)
+    {
+        cout<<"The total number of XXS surfboards is "<<iTotalXXS<<endl ;
+    }
+
 }
+void DisplayTotal(const int iTotalSmall, const int iTotalMedium, const int iTotalLarge, const int iTotalXXS)
+{
+    float sum = 0 ;
+
+    sum = iTotalSmall*175.00 + iTotalMedium*190.00 + iTotalLarge*200.00 + iTotalXXS*300;
+
+
+    if(iTotalSmall!=0)
+    {
+        cout<<"The total number of small surfboards is "<<iTotalSmall<<" at a total of $"<<iTotalSmall*175.00<<endl ;
+    }
+    if(iTotalMedium!=0)
+    {
+        cout<<"The total number of medium surfboards is "<<iTotalMedium<<" at a total of $"<<iTotalMedium*190.00<<endl ;
+    }
+    if(iTotalLarge!=0)
+    {
+        cout<<"The total number of large surfboards is "<<iTotalLarge<<" at a total of $"<<iTotalLarge*200.00<<endl ;
+    }
+    if(iTotalXXS!=0)
+    {
+        cout<<"The total number of XXS surfboards is "<<iTotalXXS<<" at a total of $"<<iTotalLarge*200.00<<endl ;
+    }
+    if((iTotalSmall== 0)&&(iTotalMedium==0)&&(iTotalLarge==0)&&(iTotalXXS==0))
+    {
+        cout<<"No purchases made yet."<<endl ;
+    }
+    else
+    {
+        cout<<"Amount due : $"<<sum<<endl ;
+    }
+}
+
+
+
+
 int main()
 {
- int iTotalSmall = 175 ;
-int& small= iTotalSmall ; 
-    cout<<setw(59)<<cout.fill('*')<<endl;
-    cout<<setw(5)<<cout.fill('*')<<" Welome to my Johnny Utah's PoinBreak Surf Shop "<<setw(5)<<cout.fill('*')<<endl;
-    cout<<setw(58)<<cout.fill('*')<<endl<<endl<<endl;
-    ShowUsage();
-    char choice;
-    //do while loop to still take valuesw from the user till he get out or to exit and quit the program
-    do{
-        cout<<"Please enter selection:";
-        cin>>choice;
-        //swich statement to check the char entered to idenify the choice of the user from the driven menu
-        switch(tolower(choice)){
-            case 's':
-                 ShowUsage();//call function
+    cout<<"*******************"<<endl ;
+    cout<<"**Welcome to my Jhonny Utah's PointBreak SurfShop**"<<endl ;
+    cout<<"*******************"<<endl ;
+
+    int iTotalSmall = 0, iTotalMedium = 0, iTotalLarge = 0, iTotalXXS = 0 ;
+
+    char selection ;
+
+    ShowUsage() ;
+    do
+    {
+        cin>>selection ;
+
+        switch(tolower(selection))
+        {
+        case 's':
+            ShowUsage();//call function
             break;
-            case 'p':
-                MakePurchase(small);//call function//call function//call function
+        case 'p':
+            MakePurchase(iTotalSmall,iTotalMedium, iTotalLarge, iTotalXXS);  //call function//call function//call function
             break;
-            case 'c':
-                DisplayPurchase(iTotalSmall );//call function//call function
+        case 'c':
+            DisplayPurchase(iTotalSmall, iTotalMedium, iTotalLarge, iTotalXXS);  //call function//call function
             break;
-            case 't':
-                DisplayTotal(iTotalSmall );//call function
+        case 't':
+            DisplayTotal(iTotalSmall, iTotalMedium, iTotalLarge, iTotalXXS);   //call function
             break;
-            
         }
-    }while(tolower(choice)!='q');//the condition to get out of the loop
-    cout<<"Thank you";
+    }
+    while(tolower(selection)!='q') ;
     return 0;
-} 
+}
